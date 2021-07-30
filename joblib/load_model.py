8799fc716d3b21befe43a8ab3f11ddea1eef10bb
Likita Raikar <likiraikar@gmail.com>
2021-07-30 19:34:22 +0530

    load a model-pickle

---
+++ joblib/load_model.py
@@ -0,0 +1,9 @@
+import joblib
+
+loaded_model=joblib.load('dib_79.pkl')
+pred=loaded_model.predict([[10,20,30,40,50,10,20,10]])
+if pred[0]==1:
+    print('the person is diabetic')
+else:
+    print('the person not is diabetic')
+
